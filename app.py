bafd5b2842849b71e554e71832c7adab022ee190
Eric <ericlg.link@gmail.com>
2023-02-06 19:11:11 -0800

    Connected db

--- app.py
+++ app.py
@@ -8,14 +8,14 @@ app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+app.config.from_pyfile('config/config.cfg')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 #Initialize Database
 db = SQLAlchemy(app)
 
 # Initialize Marshmallow
-ma = Marshmallow(app)
+ma = Marshmallow
 
 # Run Server
 if __name__ == '__main__':
