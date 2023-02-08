6059ab81d4497333e4fa5263f6f399aa9217ef76
Tvoir <TommyVoir@yahoo.com>
2023-02-07 19:17:16 -0800

    test

--- website/auth.py
+++ website/auth.py
@@ -1,16 +1,35 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, flash
 
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return "<p>LOGIN</p>"
+    data = request.form
+    print(data)
+    return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
     return "<p>LOGOUT<p/>"
 
-@auth.route('/signup')
+@auth.route('/signup', methods=['GET', 'POST'])
 def signup():
-    return "<p>SIGN UP</p>"
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be greater than 4 characters.', category='error')
+        elif len(firstName) < 2:
+            flash('Email must be greater than 1 characters.', category='error')
+        elif password1 != password2:
+            flash('passwords dont match!', category='error')
+        elif len(password1) < 7:
+            flash('Email must be greater than 7 characters.', category='error')
+        else: 
+            flash('Account Successfully created!', category='success')
+            
+    return render_template("signup.html")
